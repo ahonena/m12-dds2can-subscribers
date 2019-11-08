15bb5816dbea157e73913d44955ba1f7425df903
ahonena <andrei.ahonen@tuni.fi>
2019-11-08 16:02:48 +0200

    Starting the project...

---
+++ src/subscribe_and_write2CAN.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "subscribe_and_write2CAN.hpp"
+
+int main(){
+  std::cout << "subscribe_and_write2CAN" << std::endl;
+}
---
+++ src/subscribe_and_write2CAN.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <dds/domain/ddsdomain.hpp>
+#include <dds/core/ddscore.hpp>
+#include <dds/topic/ddstopic.hpp>
+#include <dds/pub/ddspub.hpp>
+#include <dds/sub/ddssub.hpp>
+
+#include "M12_Commands.hpp"
