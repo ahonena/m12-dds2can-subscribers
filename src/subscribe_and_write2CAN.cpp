f8578526c9ab9b739aaa4f874fb8e7d237ff6c43
ahonena <andrei.ahonen@tuni.fi>
2019-11-19 16:39:00 +0200

    Removed old .cpp files

--- src/subscribe_and_write2CAN.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "subscribe_and_write2CAN.hpp"
-
-int main(){
-  std::cout << "subscribe_and_write2CAN" << std::endl;
-}
--- src/subscribe_and_write2CAN.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <dds/domain/ddsdomain.hpp>
-#include <dds/core/ddscore.hpp>
-#include <dds/topic/ddstopic.hpp>
-#include <dds/pub/ddspub.hpp>
-#include <dds/sub/ddssub.hpp>
-
-#include "M12_Commands.hpp"
